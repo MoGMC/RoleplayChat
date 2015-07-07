4a8495b13a2c378f42afee1c89db7a8ea85b25bd
Hilaryoi <seawee65@live.com>
2015-07-07 11:05:19 -0400

    switched to maven

--- src/com/normarthehero/plugin/RoleplayChat.java
+++ src/com/normarthehero/plugin/RoleplayChat.java
@@ -1,7 +1,6 @@
-package nomarthehero;
-import java.util.ArrayList;
-import java.util.HashSet;
+package com.normarthehero.plugin;
 
+import java.util.ArrayList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -16,124 +15,120 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+public class RoleplayChat extends JavaPlugin implements Listener, CommandExecutor {
 
-public class RoleplayChat extends JavaPlugin implements Listener, CommandExecutor  {
-	
-	//Every 5~10 min "Hey <player>, you know you're still talking in roleplay chat?" (async runnable)
-	
+	// Every 5~10 min
+	// "Hey <player>, you know you're still talking in roleplay chat?" (async
+	// runnable)
+
 	private ArrayList<String> roleplayers = new ArrayList<String>();
 	private ArrayList<String> staff = new ArrayList<String>();
-	
-	
+
 	public void onEnable() {
-		
+
 		getCommand("roleplay").setExecutor(this);
 		getCommand("rp").setExecutor(this);
 		getServer().getPluginManager().registerEvents(this, this);
-		
+
 	}
-	
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-		
+
 		if (command.getName().equalsIgnoreCase("roleplay") || command.getName().equalsIgnoreCase("rp")) {
-			
-			Player player = (Player)sender;
+
+			Player player = (Player) sender;
 			String playerName = player.getName();
-			
+
 			if (roleplayers.contains(playerName)) {
-				
+
 				roleplayers.remove(playerName);
 				player.sendMessage(ChatColor.YELLOW + "Disabled roleplay chat!");
-				
-			}	else {
-				
+
+			} else {
+
 				roleplayers.add(playerName);
 				player.sendMessage(ChatColor.YELLOW + "Enabled roleplay chat!");
 				player.sendMessage(ChatColor.YELLOW + "Roleplayers will have a yellow chat, and their messages " + ChatColor.RED + "won't" + ChatColor.YELLOW + " be sent in global chat.");
 				player.sendMessage(ChatColor.YELLOW + "To disable it, type /roleplay again.");
-				
+
 			}
-			
-			
+
 		}
-		
+
 		return true;
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onChat(AsyncPlayerChatEvent e) {
-		
+
 		if (e.isCancelled() == false) {
-			
+
 			Player player = e.getPlayer();
-			
+
 			if (isRoleplayer(player)) {
-				
+
 				String message = e.getMessage();
 				message = ChatColor.YELLOW + message;
-				
+
 				e.setCancelled(true);
-				
+
 				for (String playerName : roleplayers) {
-					
+
 					Bukkit.getPlayer(playerName).sendMessage(ChatColor.YELLOW + "[RP] " + player.getDisplayName() + ChatColor.YELLOW + ": " + message);
-					
+
 				}
-				
+
 				for (String playerName : staff) {
-					
+
 					if (!roleplayers.contains(playerName)) {
-						
+
 						Bukkit.getPlayer(playerName).sendMessage(ChatColor.YELLOW + "[RP] " + player.getDisplayName() + ChatColor.YELLOW + ": " + message);
-						
-					}			
-								
+
+					}
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		
+
 		Player player = e.getPlayer();
-		
+
 		if (player.hasPermission("roleplay.staff")) {
 			staff.add(player.getName());
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void onLeave(PlayerQuitEvent e) {
-		
+
 		Player player = e.getPlayer();
-		
+
 		roleplayers.remove(player.getName());
-		
-		if (player.hasPermission("roleplay.staff")) staff.remove(player.getName());
-		
+
+		if (player.hasPermission("roleplay.staff"))
+			staff.remove(player.getName());
+
 	}
-	
+
 	private boolean isRoleplayer(Player player) {
-		
+
 		if (roleplayers.contains(player.getName())) {
-			
+
 			return true;
-			
+
 		}
-		
+
 		return false;
-		
+
 	}
-	
-	
 
 }
